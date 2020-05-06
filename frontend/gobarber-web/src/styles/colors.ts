c070a882c25fec3a299a819569daf8463f4fa000
virginiarcruz <virginiarcruz@gmail.com>
2020-05-05 23:09:37 -0300

    clean files and add globalStyle

---
+++ frontend/gobarber-web/src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const purple = '#7D40E7';
+export const purpleLight = '#F4EFFC';
+export const purpleDark = '#A28FD0';
+
+export const orange = '#CB946C';
+export const orangeLight = '#FCF4EE';
+
+export const white = '#FFFFFF';
+
+export const gray = '#999999';
+export const grayDarkPrimary = '#666666';
+export const grayDark = '#444444';
+export const grayLight = '#DDDDDD';
+export const grayLighter = '#F5F5F5';
+
+export const red = '#DE3B3B';
