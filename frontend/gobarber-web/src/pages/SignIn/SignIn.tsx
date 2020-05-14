a64056832b843674f6a069b7737553dc9015ac29
virginiarcruz <virginiarcruz@gmail.com>
2020-05-13 21:32:24 -0300

    add context to signin and singout

--- frontend/gobarber-web/src/pages/SignIn/SignIn.tsx
+++ frontend/gobarber-web/src/pages/SignIn/SignIn.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -22,7 +22,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
