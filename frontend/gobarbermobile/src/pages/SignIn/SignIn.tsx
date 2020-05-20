c06e0b11c1ad7f98f7693b5c56394261254fb462
virginiarcruz <virginiarcruz@gmail.com>
2020-05-19 22:35:32 -0300

    create page sign

---
+++ frontend/gobarbermobile/src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Text>signin</Text>
+    </Container>
+  );
+};
+
+export default SignIn;
