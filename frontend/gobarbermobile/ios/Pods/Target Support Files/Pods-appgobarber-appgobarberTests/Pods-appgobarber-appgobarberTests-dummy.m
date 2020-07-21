37215fc5b051b9ea427d3f8a391f21b1e455e736
Virginia Rodrigues Cruz <virginia@MacBook-Pro-de-Virginia.local>
2020-07-20 22:32:19 -0300

    fix native project

---
+++ frontend/gobarbermobile/ios/Pods/Target Support Files/Pods-appgobarber-appgobarberTests/Pods-appgobarber-appgobarberTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_appgobarber_appgobarberTests : NSObject
+@end
+@implementation PodsDummy_Pods_appgobarber_appgobarberTests
+@end
---
+++ frontend/gobarbermobile/ios/Pods/Target Support Files/Pods-appgobarber-tvOSTests/Pods-appgobarber-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_appgobarber_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_appgobarber_tvOSTests
+@end
