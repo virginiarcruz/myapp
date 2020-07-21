a555abd6ee4370831eead50e51a9ab42c25efdcf
Virginia Rodrigues Cruz <virginia@MacBook-Pro-de-Virginia.local>
2020-07-20 23:03:00 -0300

    Removed pods folder

--- frontend/gobarbermobile/ios/Pods/Target Support Files/Pods-appgobarber-appgobarberTests/Pods-appgobarber-appgobarberTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_appgobarber_appgobarberTests : NSObject
-@end
-@implementation PodsDummy_Pods_appgobarber_appgobarberTests
-@end
--- frontend/gobarbermobile/ios/Pods/Target Support Files/Pods-appgobarber-tvOS/Pods-appgobarber-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_appgobarber_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_appgobarber_tvOS
-@end
