d8d66332ea217be617b6a3ce53b0708e2b739b8b
Virginia Cruz <virginiarcruz@gmail.com>
2020-07-21 20:10:32 -0300

    add header

--- frontend/gobarbermobile/src/pages/Dashboard/Dashboard.tsx
+++ frontend/gobarbermobile/src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import { View, Button, Vibration } from 'react-native';
+import React, { useCallback } from 'react';
 
 import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo,{'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar
+            source={{
+              uri:
+                'https://avatars2.githubusercontent.com/u/5655363?s=460&u=1c0587ba50c4bae47bf1c8b6c6d2b95fa8968deb&v=4',
+            }}
+          />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
