1c2e5aab0b61b994ccf69ed079fe8a89f9941565
Virginia Cruz <virginiarcruz@gmail.com>
2020-07-21 21:04:29 -0300

    create appointment page

--- frontend/gobarbermobile/src/pages/CreateAppointment/CreateAppointment.tsx
+++ frontend/gobarbermobile/src/pages/CreateAppointment/CreateAppointment.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import { View, Button, Vibration } from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
+import Icon from 'react-native-vector-icons/Feather';
+import { useRoute, useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}></View>;
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+
+  const { providerId } = route.params as RouteParams;
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={goBack}>
+          <Icon name="chevron-left" size={14} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiro</HeaderTitle>
+        <UserAvatar
+          source={{
+            uri:
+              'https://avatars2.githubusercontent.com/u/5655363?s=460&u=1c0587ba50c4bae47bf1c8b6c6d2b95fa8968deb&v=4',
+          }}
+        />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
