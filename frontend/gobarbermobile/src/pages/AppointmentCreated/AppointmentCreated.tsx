76e78fd0da442ee825c3e0783622ac38a252bd51
Virginia Cruz <virginiarcruz@gmail.com>
2020-07-27 22:33:22 -0300

    success screen

--- frontend/gobarbermobile/src/pages/AppointmentCreated/AppointmentCreated.tsx
+++ frontend/gobarbermobile/src/pages/AppointmentCreated/AppointmentCreated.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import { View, Button, Vibration } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
+import { format } from 'date-fns';
+import ptbr from 'date-fns/locale/pt-BR';
 
-import { useAuth } from '../../hooks/auth';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styled';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}></View>;
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptbr },
+    );
+  }, [routeParams.date]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description> {formattedDate}</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
